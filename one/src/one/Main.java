4c374d597d854c4434fe098998ece9da6cf80daf
zedmi <923503884@qq.com>
2020-10-04 18:49:35 -0400

    initial commit

---
+++ one/src/one/Main.java
@@ -0,0 +1,20 @@
+package one;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		for(int i=0;i<10;++i) {
+			System.out.println(i);
+			if(i==3) {
+				i+=1;
+				continue;
+			}
+		}
+		
+	}
+}
